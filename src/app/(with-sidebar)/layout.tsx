1f0b5d73bffcf6069d5d1fed46ff4372f1e75049
lukemccrae <lukemccrae@gmail.com>
2025-10-31 12:22:47 -0700

    sidebar shows

--- src/app/(with-sidebar)/layout.tsx
+++ src/app/(with-sidebar)/layout.tsx
@@ -1,14 +1,14 @@
+"use client";
 import React from "react";
 import Sidebar from "../../components/Sidebar";
 
-// Layout applied to all pages under app/(with-sidebar)/...
 export default function WithSidebarLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-1 min-h-0 flex overflow-hidden">
+    <div className="flex h-full w-full min-h-0">
       <Sidebar />
-      <main className="flex-1 min-h-0 overflow-auto">
+      <div className="flex-1 min-h-0 overflow-auto">
         {children}
-      </main>
+      </div>
     </div>
   );
 }
